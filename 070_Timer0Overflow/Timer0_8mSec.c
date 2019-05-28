f97d0af79e6589ef5f3338c731ba39ad52e362b8
Soochan Kim <sckim@hknu.ac.kr>
2019-05-28 22:46:44 +0900

    Updata

---
+++ 070_Timer0Overflow/Timer0_8mSec.c
@@ -0,0 +1,70 @@
+/*************************************
+ * Purpose: Timer0을 이용하여 1초마다 overflow
+ * interrupt에 의한 LED Shift
+ *
+ * TCCR0A
+ * TCCR0B
+ * TCNT0
+ *************************************/
+/*************************************
+ * Purpose: Timer0을 이용하여 1초마다 overflow
+ * interrupt에 의한 LED Shift
+ *
+ * TCCR0A
+ * TCCR0B
+ * TCNT0
+ *************************************/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// from TCNT = (CS / 16000000 ) * (256-x) =10msec
+// x = time * (16000000/CS)
+// 15625*8msec = 125
+#define cDelay 256-125
+
+volatile int sec = 0;
+volatile int msec8 = 0;
+
+int main(void) {
+	char s10, s1;
+
+	DDRD = 0xFF;
+	DDRB |= _BV(PB5);
+
+	PORTD = (sec << 4) + msec8;
+
+	TCCR0A = 0;
+
+	// CS0[2:0]
+	// 000 : No clock source
+	// 001 : clk/1
+	// 010 : clk/8
+	// 011 : clk/64
+	// 100 : clk/256
+	// 101 : clk/1024
+	TCCR0B |= (1 << CS02);	// Clock/1024
+	TCCR0B |= (1 << CS00);	// Clock/1024
+
+	TCNT0 = cDelay;
+	while (1) {
+		// Check overflow flag
+		if (bit_is_set(TIFR0, TOV0)) {
+			// reset the overflow flag
+			TIFR0 |= _BV(TOV0);
+			TCNT0 = cDelay;
+
+			msec8++;
+			if (msec8 == 125) {
+				sec++;
+				msec8 = 0;
+				s10 = sec / 10;
+				s1 = sec % 10;
+				PORTD = (s10 << 4) + s1;
+				PORTB ^= _BV(PB5);
+			}
+			if (sec == 99) {
+				sec = 0;
+			}
+		}
+	}
+}
