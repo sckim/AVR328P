d69aaa07cb25be8e1b35e268392b2310ed044b3a
Soochan Kim <sckim@hknu.ac.kr>
2019-07-22 18:13:37 +0900

    Update GLCD

--- 777_Allfunction/gpio.c
+++ 777_Allfunction/gpio.c
@@ -140,3 +140,18 @@ void checkGPIO(void) {
 		_delay_ms(100);
 	}
 }
+
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
+{
+	uint8_t i;
+
+	for (i = 0; i < 8; i++)  {
+		if (bitOrder == LSBFIRST)
+			digitalWrite(dataPin, !!(val & (1 << i)));
+		else
+			digitalWrite(dataPin, !!(val & (1 << (7 - i))));
+
+		digitalWrite(clockPin, HIGH);
+		digitalWrite(clockPin, LOW);
+	}
+}
--- 777_Allfunction/gpio.h
+++ 777_Allfunction/gpio.h
@@ -8,9 +8,13 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+#include <stdio.h>
+
 void pinMode(uint8_t pin, uint8_t mode);
 void digitalWrite(uint8_t pin, uint8_t val);
 int digitalRead(uint8_t pin);
 void checkGPIO(void);
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+
 
 #endif /* GPIO_H_ */
