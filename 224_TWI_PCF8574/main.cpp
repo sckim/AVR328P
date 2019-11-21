04c47b25d392b3f19fa5bcdd440b8bb64e6a038c
sckim <dr.soochankim@gmail.com>
2019-11-21 11:06:42 +0900

    Update main.cpp

--- 224_TWI_PCF8574/main.cpp
+++ 224_TWI_PCF8574/main.cpp
@@ -5,16 +5,18 @@
  *      Author: Soochan Kim
  */
 
+#define F_CPU	16000000UL
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include "PCF8574.h"
-
-#define F_CPU	16000000UL
 
 //0x38
 PCF8574 PCF_01(0x40);
 
 int main(void) {
+
+	DDRD |= _BV(PD7);
 
 	PCF_01.begin();
 
@@ -32,6 +34,7 @@ int main(void) {
 
 	PCF_01.digitalWrite(7, 1);
 	_delay_ms(1000);
+	
 	for (int i = 0; i < 7; i++) {
 		PCF_01.shiftRight(1);
 		_delay_ms(100);
@@ -45,9 +48,22 @@ int main(void) {
 		_delay_ms(100);
 	}
 
+	unsigned char a;
+
 	while (1) {
-		PORTD = PCF_01.digitalReadAll();
-		_delay_ms(1000);
+//		a = PCF_01.digitalReadAll();
+//		if (a&0x80)
+//			PORTD |= 0x80;
+//		else
+//			PORTD &= ~0x80;
+
+		a = PCF_01.digitalRead(7);
+		if(a)
+			PORTD |= 0x80;
+		else
+			PORTD &= ~0x80;
+
+		_delay_ms(10);
 	}
 
 	return 0;
