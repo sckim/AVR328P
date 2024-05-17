8c9ba373b9bfce16a51e80af45f129db4fae91e2
Soochan Kim <dr.soochankim@gmail.com>
2024-05-17 17:40:53 +0900

    Update some files

--- 010_Blink/src/Blink.c
+++ 010_Blink/src/Blink.c
@@ -9,6 +9,26 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/sfr_defs.h>
+#include <avr/interrupt.h>
+
+//void blink(void)
+ISR(INT1_vect)  
+{
+	PORTB ^= _BV(cLED);
+//    PORTB = PORTB ^ _BV(cLED);
+/*    
+    if(PORTB &_BV(cLED))
+       PORTB &= ~_BV(cLED);
+    else
+       PORTB |= _BV(cLED);
+
+  // ^: XOR
+  // |: OR
+  // &: AND
+  // ~: NOT
+*/  
+}
 
 int main(void) {
 	DDRB = 0xFF;
@@ -20,6 +40,7 @@ int main(void) {
 		PORTB &= ~_BV(PB5);
 		//PORTB &= ~(1<<PB5);
 		_delay_ms(1000);
+		bit_is_set(PORTB, PB5);
 	}
 	return 0;
 }
--- 060_UART/src/main.cpp
+++ 060_UART/src/main.cpp
@@ -115,3 +115,30 @@ int main(void) {
 	return 0; // never reached
 }
 
+/*
+ Analog input reads an analog input on analog in 0, prints the value out.
+ created 24 March 2006
+ by Tom Igoe
+ */
+
+int analogValue = 0;    // variable to hold the analog value
+
+void setup() {
+  // open the serial port at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog input on pin 0:
+  analogValue = analogRead(0);
+
+  // print it out in many formats:
+  Serial.println(analogValue);       // print as an ASCII-encoded decimal
+  Serial.println(analogValue, DEC);  // print as an ASCII-encoded decimal
+  Serial.println(analogValue, HEX);  // print as an ASCII-encoded hexadecimal
+  Serial.println(analogValue, OCT);  // print as an ASCII-encoded octal
+  Serial.println(analogValue, BIN);  // print as an ASCII-encoded binary
+
+  // delay 10 milliseconds before the next reading:
+  delay(10);
+}
